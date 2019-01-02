51fc525ae9ee052b3756c514d04d6bf8f8afe6db
guillaumeBianchi <g.bianchi@hotmail.fr>
2019-01-02 17:42:39 +0100

    textgame v1

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textComponent;
+
+    [SerializeField] State startingState;
+
+    State state;
+
+    void Start()
+    {
+        DisplayStoryText(startingState);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            DisplayStoryText(state.NextStates[0]);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(state.NextStates.Length > 1)
+                DisplayStoryText(state.NextStates[1]);
+        }
+    }
+
+    private void DisplayStoryText(State _state)
+    {
+        state = _state;
+        textComponent.text = state.StoryText;
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)] [SerializeField] string storyText;
+
+    [SerializeField] State[] nextStates;
+
+    public string StoryText
+    {
+        get { return storyText; }
+    }
+
+    public State[] NextStates
+    {
+        get { return nextStates; }
+    }
+}
